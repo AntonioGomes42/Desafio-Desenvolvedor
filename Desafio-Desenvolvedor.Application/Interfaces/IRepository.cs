4f629379c037c672dcefb142050263547fdc64e3
AntonioGomes42 <br.com.antoniogomes@gmail.com>
2025-09-18 08:39:54 -0300

    * Adicionando camada de 'Application' com as suas interfaces.

---
+++ Desafio-Desenvolvedor.Application/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Desafio_Desenvolvedor.Domain.Entities;
+
+namespace Desafio_Desenvolvedor.Application.Interfaces
+{
+    public interface IRepository : IDisposable
+    {
+        Task<List<BlogPost>> GetAllPostsAsync(CancellationToken stoppingToken = default);
+        Task CreatePostAsync(BlogPost post, CancellationToken stoppingToken = default);
+        Task<BlogPost?> GetPostByIdAsync(Guid id, CancellationToken stoppingToken = default);
+        Task AddCommentToPostAsync(Guid postId, Comment comment, CancellationToken stoppingToken = default);
+    }
+}
