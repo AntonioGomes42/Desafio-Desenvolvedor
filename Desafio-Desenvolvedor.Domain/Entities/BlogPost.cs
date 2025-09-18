51a36b7cb7ea1489abed1135385975ec96bd3257
AntonioGomes42 <br.com.antoniogomes@gmail.com>
2025-09-18 08:25:35 -0300

    * Adição do projeto 'Domain' e dos modelos de dados.

---
+++ Desafio-Desenvolvedor.Domain/Entities/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio_Desenvolvedor.Domain.Entities
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Title { get; set; }
+        public string Author { get; set; } = "Anonymous";
+        public required string Content { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+        public ICollection<Comment> Comments { get; set; } = [];
+    }
+}
---
+++ Desafio-Desenvolvedor.Domain/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafio_Desenvolvedor.Domain.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Author { get; set; } = "Anonymous";
+        public required string Text { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+        public Guid PostId { get; set; }
+    }
+}
